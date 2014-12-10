a6846b3cc5d7623488ca300e62c8d5d70c92d7f3
ilnuribat <ilnuribat@gmail.com>
2014-12-10 10:22:59 +0500

    new function - find index with min power of vertex

--- main.cpp
+++ main.cpp
@@ -5,10 +5,62 @@
 
 using namespace std;
 
+vector<vector<long>> A, S;
+vector<long> removed;
+//A - матрица растояний, S - матрица последователей
+long N;
+//Размерность
+const long pMax = 1000;
+
+//Ищем вершину с минимальным количеством смежных вершин с остальными вершинами
+//которые ещё не были удалены
+long findMinVert()
+{
+	long Min = N, iMin;
+	for (int i = 0; i < N; i++)
+		if (removed[i] == 0)
+		{
+			iMin = i;
+			break;
+		}
+
+	for (int i = 0; i < N; i++)
+	{
+		if (removed[i] == 0)
+		{
+			long count = 0;
+			for (int j = 0; j < N; j++)
+			{
+				if (removed[j] == 0 && A[i][j] < pMax)
+					count++;
+			}
+			if (count < Min){
+				Min = count;
+				iMin = i;
+			}
+		}
+	}
+	return 0;
+}
 int main()
 {
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
+	cin >> N;
+	A.resize(N);
+	S.resize(N);
+	removed.resize(N);
+	
+	for (int i = 0; i < N; i++)
+	{
+		A[i].resize(N);
+		S[i].resize(N);
+		for (int j = 0; j < N; j++){
+			cin >> A[i][j];
+			if (A[i][j] < 0)
+				A[i][j] = pMax;
+		}
+	}
 
 	return 0;
 }
