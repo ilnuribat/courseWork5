acc99a51ebf2830b02efdb3e51acbea1249f637b
ilnuribat <ilnuribat@gmail.com>
2014-12-10 11:19:54 +0500

    deleting vertex done

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 #include <cmath>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 vector<vector<long>> A, S;
 vector<long> removed;
+vector<long> neighbohoor;
 //A - матрица расто€ний, S - матрица последователей
 long N;
 //–азмерность
@@ -48,7 +50,7 @@ long findMinVert()
 void init()
 {
 	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
+	//freopen("output.txt", "w", stdout);
 	cin >> N;
 	A.resize(N + 1);
 	S.resize(N + 1);
@@ -69,14 +71,41 @@ void init()
 // —обственно говор€, само решение
 void solve()
 {
+	//ѕроцесс удалени€ вершин
 	//p - пор€дковый номер удалени€ вершин
-	for (int p = 1; p < N; p++)
+	for (int p = 1; p < N - 1; p++)
 	{
 		// ¬ершина, которую будем удал€ть
 		long Min = findMinVert();
 		removed[Min] = p;
-
+		
+		//находим все смежные вершины, которые ещЄ не удалены
+		neighbohoor.clear();
+		for (int i = 1; i < N; i++)
+		{
+			// существует путь
+			if (removed[i] == 0 && A[Min][i] < pMax)
+				neighbohoor.push_back(i);
+		}
+		
+		//пробегаем по всем из них, смотрим, не выполн€етс€ ли правило треугольника
+		vector<long>::iterator I, J;
+		for (I = neighbohoor.begin(); I != neighbohoor.end(); I++)
+		{
+			for (J = neighbohoor.begin(); J != neighbohoor.end(); J++)
+			{
+				
+				//не выполн€етс€ правило треугольника
+				if (A[*I][*J] > A[*I][Min] + A[Min][*J])
+					A[*I][*J] = A[*I][Min] + A[Min][*J];
+				//аналоги:
+				//A[i][j] = A[i][j] > A[i][Min] + A[Min][j] ? A[i][Min] + A[Min][j] : A[i][j];
+				//A[i][j] = min(A[i][j], A[i][Min] + A[Min][j]);
+			}
+		}
 	}
+
+	
 }
 
 int main()
