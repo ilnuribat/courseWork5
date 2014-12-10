d5cd08aa5e23430f870fc1906f1e116bdf9723bf
ilnuribat <ilnuribat@gmail.com>
2014-12-10 10:00:29 +0500

    added files

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cmath>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+
+	return 0;
+}
